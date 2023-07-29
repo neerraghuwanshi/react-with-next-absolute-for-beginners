2c46362f84d9282ed8274921f9800217b6f6ef85
Neer Raghuwanshi <raghuwanshineer1@gmail.com>
2023-07-30 01:49:11 +0530

    Explained

--- components/ChildComponent.js
+++ components/ChildComponent.js
@@ -1,26 +1,22 @@
 'use client';
 
 
-import { useEffect } from 'react'
+import { useEffect, memo } from 'react'
 
 
-function ChildComponent() {
+function ChildComponent(props) {
 
     useEffect(() => {
         // Do an ajax call for eg.
         console.log('Child Mounted or Updated');
-
-        return () => {
-            // Clear the timer for example
-            console.log('Unmounting');
-        }
-    }, [])
+    })
 
     return (
+        props.show &&
         <h1>
             This was to be shown
         </h1>
     )
 }
 
-export default ChildComponent
+export default memo(ChildComponent)
--- components/ParentComponent.js
+++ components/ParentComponent.js
@@ -1,13 +1,22 @@
 'use client';
 
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ChildComponent from './ChildComponent';
 
 
 function ParentComponent() {
 
     const [show, setShow] = useState(false)
+
+    // Inse bhi update ho jata hai component
+    // useSelector()
+    // useReducer()
+
+    useEffect(() => {
+        // Do an ajax call for eg.
+        console.log('Parent Mounted or Updated');
+    })
 
     const onClickHandler = () => {
         setShow(prev => !prev)
@@ -18,10 +27,7 @@ function ParentComponent() {
             <button onClick={onClickHandler}>
                 Toggle
             </button>
-            {
-                show &&
-                <ChildComponent />
-            }
+            <ChildComponent show={show}/>
         </>
     )
 }
